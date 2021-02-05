53002709137e9880e450e2f43ed2c0bd0afedce6
Altair Sossai <altairsossai@gmail.com>
2021-02-05 17:10:35 -0300

    Inicio do projeto

---
+++ Zeats.Error.FluentValidation/Extensions/ValidationExceptionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using FluentValidation;
+
+namespace Zeats.Error.FluentValidation.Extensions
+{
+    public static class ValidationExceptionExtensions
+    {
+        public static ErrorInfo ToErrorInfo(this ValidationException exception)
+        {
+            return new ErrorInfo
+            {
+                Severity = Severity.Error,
+                Type = "Validation",
+                Message = exception.Message,
+                Details = exception.ToString(),
+                Errors = exception.Errors?.Select(error => error.ToErrorItem()).ToList()
+            };
+        }
+    }
+}
---
+++ Zeats.Error.FluentValidation/Extensions/ValidationFailureExtensions.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation.Results;
+
+namespace Zeats.Error.FluentValidation.Extensions
+{
+    public static class ValidationFailureExtensions
+    {
+        public static ErrorItem ToErrorItem(this ValidationFailure validationFailure)
+        {
+            return new ErrorItem
+            {
+                Severity = (Severity) validationFailure.Severity,
+                Type = "Validation",
+                Property = validationFailure.PropertyName,
+                Message = validationFailure.ErrorMessage
+            };
+        }
+    }
+}
